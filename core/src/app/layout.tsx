38ab69f43f4c0a1319d7cef0d2ffa143a170d9e1
unknown <hisamutdinovalan39@gmail.com>
2024-05-25 15:05:48 +0500

    meta

--- core/src/app/layout.tsx
+++ core/src/app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="yandex-verification" content="7d1f6d2e81411212" />
+        <meta name="yandex-verification" content="022511a094aaa775" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favi.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favi.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favi.png" />
