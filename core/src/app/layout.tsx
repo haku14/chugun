3e3f886821545e9d7e45b918b3aadacc99637213
unknown <hisamutdinovalan39@gmail.com>
2024-05-26 19:35:02 +0500

    eduts

--- core/src/app/layout.tsx
+++ core/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "itsupufa.ru",
+  description: "",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
--- core/src/components/Footer/index.tsx
+++ core/src/components/Footer/index.tsx
@@ -11,6 +11,7 @@ export const Footer = ({
   modal: boolean;
   setModal: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const [doModal, setDoModal] = useState(false);
   return (
     <>
       <div className="mt-[130px] bg-[#1E212C] w-full flex justify-center text-white py-20">
@@ -66,11 +67,37 @@ export const Footer = ({
             <input type="text" placeholder="Номер телефона" />
             <textarea placeholder="Комментарий"></textarea>
             <button
-              onClick={() => setModal(false)}
+              onClick={() => {
+                setModal(false);
+                setDoModal(true);
+              }}
               className="w-full bg-[#7772F1] py-3 rounded-[4px] text-white"
             >
               Оставить заявку
             </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className={twMerge(
+          "fixed z-[100] top-0 left-0 w-screen h-screen flex items-center bg-black bg-opacity-35 justify-center opacity-0 pointer-events-none duration-300",
+          doModal && "opacity-100 pointer-events-auto"
+        )}
+      >
+        <div className="max-w-[718px] w-full p-4 rounded-[4px] bg-white">
+          <div className="flex justify-between items-center">
+            <p className="text-3xl font-bold">
+              Ваша заявка успешно отправлена менеджерам, с вами свяжутся в
+              ближайшее время
+            </p>
+            <Image
+              onClick={() => setDoModal(false)}
+              className="cursor-pointer"
+              src={"/icons/cross.svg"}
+              alt="cross"
+              width={45}
+              height={45}
+            />
           </div>
         </div>
       </div>
